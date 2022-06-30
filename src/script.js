b9a9b91365c3b466b5f836b0452a6054b3de5cfd
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-06-30 14:15:44 -0300

    Add script ao html, criar função positions e console.log da mesma aplicada ao array

--- src/script.js
+++ src/script.js
@@ -1,12 +1,15 @@
 let names = ["Rafael", "Manoel", "Daniel"];
 function positions(arr){
-    if(arr.indexOf("Daniel") === 0) {
-        return "Daniel é o vencedor";
-    } else {
-        let originalPosition = arr.indexOf("Daniel");
-        let aux = arr[originalPosition - 1];
+    const str = "Este é o pódio";
+    if(arr.indexOf("Daniel") != 0) {
+        const originalPosition = arr.indexOf("Daniel");
+        const aux = arr[originalPosition - 1];
         arr[originalPosition - 1] = "Daniel";
         arr[originalPosition] = aux;
-        return "Este é o pódio".concat(`1° ${arr[0]} 2° ${arr[1]} 3° ${arr[2]}`);
+        return str.concat(`: 1° ${arr[0]} 2° ${arr[1]} 3° ${arr[2]}`);
+        
+    } else {
+        return "Daniel é o vencedor";
     }
 }
+console.log(positions(names));
