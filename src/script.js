4bb7c80865fc65263326f5714effff2b96f9bac7
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-06-30 14:03:22 -0300

    Criando a função positions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
 let names = ["Rafael", "Manoel", "Daniel"];
+function positions(arr){
+    if(arr.indexOf("Daniel") === 0) {
+        return "Daniel é o vencedor";
+    } else {
+        let originalPosition = arr.indexOf("Daniel");
+        let aux = arr[originalPosition - 1];
+        arr[originalPosition - 1] = "Daniel";
+        arr[originalPosition] = aux;
+        return "Este é o pódio".concat(`1° ${arr[0]} 2° ${arr[1]} 3° ${arr[2]}`);
+    }
+}
